43235819b540230525c7970acd7ee9435c69bec6
umarabdullah <37032077+umarabdullah@users.noreply.github.com>
2018-03-11 12:29:24 +0800

    Add files via upload

---
+++ ReadNameFromExcel.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.stiw3054_assignment.quiz1xcel;
+
+/**
+ *
+ * @author abdullah
+ */
+
+
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+
+
+public class ReadNameFromExcel {
+
+    private static final String FILE_NAME = "name.xlsx";
+
+    public static void main(String[] args)throws IOException {
+//    File f = new File(FILE_NAME);
+      readXLSXFile();
+    }
+    public static void readXLSXFile() throws IOException
+	{
+		InputStream ExcelFileToRead = new FileInputStream(FILE_NAME);
+		XSSFWorkbook  wb = new XSSFWorkbook(ExcelFileToRead);
+		
+		XSSFWorkbook test = new XSSFWorkbook(); 
+		
+		XSSFSheet sheet = wb.getSheetAt(0);
+		XSSFRow row; 
+		XSSFCell cell;
+
+		Iterator rows = sheet.rowIterator();
+
+		while (rows.hasNext())
+		{
+			row=(XSSFRow) rows.next();
+			Iterator cells = row.cellIterator();
+			while (cells.hasNext())
+			{
+				cell=(XSSFCell) cells.next();
+		
+				if (cell.getCellType() == XSSFCell.CELL_TYPE_STRING)
+				{
+					System.out.print(cell.getStringCellValue()+" ");
+				}
+				
+			}
+			System.out.println();
+		}
+	
+	}
+}
